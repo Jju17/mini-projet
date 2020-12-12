ff7b357fe72c255ec4623503f595e8a65537e834
Julien <julien.rahier@gmail.com>
2020-12-12 18:40:28 +0100

    test

--- js/cv.js
+++ js/cv.js
@@ -5,7 +5,9 @@ const cvHTML = `
 
 fetch("../fixtures/datacv.json")
   .then(response => response.json())
-  .then(json => console.log(json));
+  .then(json => console.log(json))
+  .catch(err => console.log(err));
+
 
 document.getElementById("cvButton").addEventListener("click", () => {
     document.getElementById("mainContent").innerHTML = cvHTML;
